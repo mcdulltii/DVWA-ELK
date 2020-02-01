ee3e4aa83ff22dc67d697ef8b4a193758149588a
mcdulltii <mcdulltii@gmail.com>
2020-02-01 20:54:04 +0800

    base

---
+++ dvwamysql/start_service.sh
@@ -0,0 +1,19 @@
+service filebeat start && \
+service metricbeat start && \
+service heartbeat-elastic start && \
+filebeat modules enable suricata && \
+filebeat setup --dashboards -E output.elasticsearch.hosts=['elasticsearch:9200'] \
+-E output.elasticsearch.username="elastic" \
+-E output.elasticsearch.password="TTF8notmD726DgbAS3an" \
+-E setup.kibana.host=kibana:5601
+
+metricbeat setup --dashboards -E output.elasticsearch.hosts=['elasticsearch:9200'] \
+-E output.elasticsearch.username="elastic" \
+-E output.elasticsearch.password="TTF8notmD726DgbAS3an" \
+-E setup.kibana.host=kibana:5601
+
+heartbeat setup -E output.elasticsearch.hosts=['elasticsearch:9200'] \
+-E output.elasticsearch.username="elastic" \
+-E output.elasticsearch.password="TTF8notmD726DgbAS3an" \
+-E setup.kibana.host=kibana:5601
+
