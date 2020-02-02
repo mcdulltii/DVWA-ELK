264def49290df52ab8f3ec3534d8159c23f4ff6b
mcdulltii <mcdulltii@gmail.com>
2020-02-02 17:30:12 +0800

    fixes

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ docker-compose build && \
 docker-compose up -d && \
 sleep 1m
 docker exec -t dvwa-elk_dvwamysql_1 chmod +x ./start_service.sh &
-docker exec -t dvwa-elk_dvwamysql_1 ./start_service.sh &
+docker exec -t dvwa-elk_dvwamysql_1 bash &
 sleep 2m
 disown
 docker exec -t dvwa-elk_dvwa_1 chmod +x ./start_service.sh
-docker exec -t dvwa-elk_dvwa_1 ./start_service.sh &
+docker exec -t dvwa-elk_dvwa_1 bash &
