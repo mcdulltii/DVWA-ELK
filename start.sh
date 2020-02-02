9e62cc42dfb135ca21aeb48a7cacab9f1f4b7e93
mcdulltii <mcdulltii@gmail.com>
2020-02-02 17:04:44 +0800

    add db

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 docker-compose build && \
 docker-compose up -d && \
 sleep 1m
-docker exec -t dvwa-elk_dvwamysql_1 bash &
+docker exec -t dvwa-elk_dvwamysql_1 chmod +x ./start_service.sh &
+docker exec -t dvwa-elk_dvwamysql_1 ./start_service.sh &
 sleep 2m
 disown
-docker exec -t dvwa-elk_dvwa_1 bash &
+docker exec -t dvwa-elk_dvwa_1 chmod +x ./start_service.sh
+docker exec -t dvwa-elk_dvwa_1 ./start_service.sh &
